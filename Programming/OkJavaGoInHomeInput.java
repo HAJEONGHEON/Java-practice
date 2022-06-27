21cd2b9587cd8f006cf37f946b3d6467c5a58da1
hajeongheon <wjdgjs2476@naver.com>
2022-06-27 21:10:27 +0900

    Add iot programming

--- Programming/OkJavaGoInHomeInput.java
+++ Programming/OkJavaGoInHomeInput.java
@@ -1,13 +1,17 @@
 // org -> opentutorials -> iot -> Elevator, Security, Lighting 불러오기
 // import를 사용하여 데이터타입(Elevator, Security, Lighting)만 입력할 수 있게 지정
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Security;
 import org.opentutorials.iot.Lighting;	
-public class OkJavaGoInHome {
+public class OkJavaGoInHomeInput {
 
 	public static void main(String[] args) {
-		
-		String id = "MY APT 1004"; // 문자열 변수지정
+		// args : 매개변수 (parameter)
+		String id = args[0]; // 문자열 변수지정
+		String bright = args[1];
 		// Elevator call
 		Elevator myElevator = new Elevator(id);
 		myElevator.callForUp(10); // MY APT 1004 -> Elevator callForUp stopFloor : 10
@@ -23,6 +27,11 @@ public class OkJavaGoInHome {
 		Lighting floorLamp = new Lighting(id+" / Floor Lamp");
 		floorLamp.on(); // MY APT 1004 / Floor Lamp -> Lighting on
 		
+		// 무드등 밝기 조절
+		DimmingLights moodLamp = new DimmingLights(id+" / moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		// moodlamp는 setBright method에 double 데이터를 입력받아 밝기 조절
+		moodLamp.on();
 	}
 
 }
